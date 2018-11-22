d8e454a8a60e7654b6f77764d5ef5155b4333309
Alexander Emerman <alexander.emerman@cern.ch>
2018-11-22 05:25:06 -0500

    Adding ConfigFunctions to standardize config across plotting macros. Will eventually pull in BinConfig

---
+++ source/gbbCalibration/python/ConfigFunctions.py
@@ -0,0 +1,41 @@
+import sys
+import string
+import os
+from ROOT import TTree
+
+def GetDataFile(name):
+  if os.path.exists(name):
+    print "found",name
+    return name
+  data_paths = string.split(os.environ['DATAPATH'], os.pathsep)
+  found = False
+  for path in data_paths:
+    if os.path.exists(os.path.join(path,name)):
+      found = True
+      break
+  if found:
+    print "found",name,"in",path
+    return os.path.join(path,name)
+  else:
+    print name,"not found"
+    return ""
+
+def MakeCrossSectionTable():
+  filepath = GetDataFile("gbbCalibration/xsections.txt")
+  if not filepath:
+    print "Cross-sections file not found!"
+    exit()
+  tree = TTree("tree","")
+  tree.ReadFile(filepath)
+  output = {}
+  for entry in tree:
+    output[entry.dataset_number] = {'xSec': entry.crossSection, 'kFac': entry.kFactor, 'fEff': entry.genFiltEff}
+  print "Loaded cross-section information"
+  return output
+
+def LoadGlobalConfig():
+  filepath = GetDataFile("gbbCalibration/configs/BinConfig.cfg")
+  if not filepath:
+    print "Global config file not found!"
+    exit()
+  from filepath import BinConfig
