dd8fab05196001123f816191d6102e7ff01f9d4e
Alexander Emerman <alexander.emerman@cern.ch>
2018-11-30 10:08:35 -0500

    util uses positional arguments again

--- source/gbbCalibration/util/run_gbbTupleAna.cxx
+++ source/gbbCalibration/util/run_gbbTupleAna.cxx
@@ -11,58 +11,32 @@ void Usage(const string& name)
 {
   cerr << "Usage: "
        << name
-       << " -c <config> (-l list | -f file)\n"
-       << "       -c       <config> file\n"
-       << "       -l       file containing list of inputs\n"
-       << "       -f       input file\n"
-       << "       -o       output file name\n"
-       << "       -h       Print out this message"
+       << " <infile> <outfile> <configfile>\n"
+       << " If infile ends in .txt it is assumed to be a list\n"
+       << " of root files (one per line).\n"
        << endl;
 }
 
 /*---------------------------------------------------------------------------*/
 int main(int argc,char *argv[]){
-  int c;
-  std::string config;
-  std::string infile;
-  std::string inlist;
-  std::string outfile;
 
-  while ((c = getopt(argc, argv, "hc:f:l:o:")) != -1) {
-    switch (c) {
-      case 'c':
-        config = optarg;
-        break;
-      case 'f':
-        infile = optarg;
-        break;
-      case 'l':
-        inlist = optarg;
-        break;
-      case 'o':
-        outfile = optarg;
-        break;
-      case 'h':
-      default:
-        Usage(basename(argv[0]));
-        return 0;
-    }
-  }
-  // Need a config file and either an input file or input list (not both)
-  bool useList = !inlist.empty();
-  if ( config.empty() || outfile.empty() || (!useList && infile.empty()) || (useList && !infile.empty()) ) {
+  if (argc < 4) {
     std::cerr << "[ERROR] (" << basename(argv[0]);
-    std::cerr << ") missing arguments: -c <config>, -o <output name> and one of -l <list> or -f <file> required." << std::endl;
+    std::cerr << ") missing arguments: <infile> <outfile> <configfile> required." << std::endl;
     Usage(basename(argv[0]));
     return 1;
   }
+  TString infile = argv[1];
+  TString outfile = argv[2];
+  TString config = argv[3];
+  bool useList = infile.EndsWith(".txt");
 
-  std::vector<TString> infiles;
+  std::vector<TString> infiles = std::vector<TString>();
   if (useList) { // Pass list of input files to GbbTupleAna
-    ifstream fs(inlist);
+    ifstream fs(infile.Data());
     if (!fs.is_open()) {
       std::cerr << "[ERROR] (" << basename(argv[0]);
-      std::cerr << ") could not open list of input files: " << inlist.c_str() << std::endl;
+      std::cerr << ") could not open list of input files: " << infile.Data() << std::endl;
       return 1;
     }
     // Assume 1 file per line, no extra formatting
