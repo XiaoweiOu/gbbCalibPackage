4b83e9886aec7e3e47369abdb438c7e340c5fe68
Tong Ou <tong.ou@cern.ch>
2019-04-12 20:11:06 -0700

    Add fitting variable correlation check

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-setupATLAS
+source /global/project/projectdirs/atlas/scripts/setupATLAS.sh
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
 cd source
 if [ -f ".asetup.save" ]; then
   asetup --restore
@@ -6,4 +8,4 @@ else
   asetup AnalysisBase,21.2.35,here
 fi
 cd ..
-source build/${AnalysisBase_PLATFORM}/setup.sh
+source build/x86_64-suse123-gcc62-opt/setup.sh
