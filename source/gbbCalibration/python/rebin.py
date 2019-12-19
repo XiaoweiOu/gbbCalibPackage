1204e7512dbd6e13b4c534d57aa9d2f8e7d3f015
Alexander Emerman <alexander.emerman@cern.ch>
2019-12-19 14:33:19 -0500

    Add option to rebin positive and negative tails separately

--- source/gbbCalibration/python/rebin.py
+++ source/gbbCalibration/python/rebin.py
@@ -14,6 +14,8 @@ parser.add_argument('--force', type=int, default=1,
                     help="Force rebinning by at least n [default: 1]")
 parser.add_argument('--hist', type=str, default='BB',
                     help="Changes which hists are checked for stat threshold. Options are 'data', 'MC', or any of the flavour-pair labels [default: BB].")
+parser.add_argument('--split', action='store_true',
+                    help="Rebin negative tail separately (merging from right to left)")
 args = parser.parse_args()
 
 # import ROOT after argument parsing so it can't steal arguments
@@ -62,7 +64,7 @@ ListOfPlotVars = MyConfig.GetPlotVariables()
 ListOfFJpt = MyConfig.GetFatJetRegions()
 ListOfTJpt = MyConfig.GetDiTrkJetRegions()
 
-def GetBinsByStats(hists,thr,n_f):
+def GetBinsByStats(hists,thr,n_f,split=False):
   '''
   Define a new binning for input histograms such that
   (bin error)/(bin value) < threshold for all bins
@@ -73,43 +75,74 @@ def GetBinsByStats(hists,thr,n_f):
   '''
   # TODO: First do some basic sanity checks
 
-  # Add bins below imin to output, keeping them unchanged
-  bins = [ hists[0].GetBinLowEdge(1) ]
-
+  nbins = hists[0].GetNbinsX()+1
   # Main body of the algorithm
   # Loops over histogram bins storing contents and sum of squares of errors
   # When it find a bin (or combination of bins) with err/val < threshold
   # it stores a new bin edge and resets the counter
   # Also requires that all bins have non-zero contents
-  nbins = hists[0].GetNbinsX()+1
-  val = np.zeros(len(hists))
-  err2 = np.zeros(len(hists))
-  ctr = 0
-  for ibin in range(1,nbins):
-    ctr += 1
-    make_bin = True
-    for ih in range(0,len(hists)):
-      val[ih]  += hists[ih].GetBinContent(ibin)
-      err2[ih] += hists[ih].GetBinError(ibin)*hists[ih].GetBinError(ibin)
-      if (val[ih] < 1e-6 or np.sqrt(err2[ih])/val[ih] > thr or ctr < n_f):
-        make_bin = False
-    # if conditions are passed for all histograms then define a new bin edge
-    if make_bin:
-      bins.append( hists[0].GetBinLowEdge(ibin+1) )
-      val[...] = 0
-      err2[...] = 0
-      ctr = 0
-
-  # if last bin would fail threshold, append it to second-to-last
-  high_edge = hists[0].GetBinLowEdge(nbins)
-  if high_edge not in bins:
-    # edge case : histogram has only 1 bin left
-    if len(bins) == 1:
-      bins.append(high_edge)
+  def GetBinningInRange(imin,imax,scanRight=True):
+    val = np.zeros(len(hists))
+    err2 = np.zeros(len(hists))
+    ctr = 0
+    if scanRight:
+      rng = range(imin,imax)
+      bins = [ hists[0].GetBinLowEdge(imin) ]
     else:
-      bins[len(bins)-1] = high_edge
+      rng = range(imax,imin,-1)
+      bins = [ hists[0].GetBinLowEdge(imax+1) ]
 
-  return bins
+    for ibin in rng:
+      ctr += 1
+      make_bin = True
+      for ih in range(0,len(hists)):
+        val[ih]  += hists[ih].GetBinContent(ibin)
+        err2[ih] += hists[ih].GetBinError(ibin)*hists[ih].GetBinError(ibin)
+        if (val[ih] < 1e-6 or np.sqrt(err2[ih])/val[ih] > thr or ctr < n_f):
+          make_bin = False
+      # if conditions are passed for all histograms then define a new bin edge
+      if make_bin:
+        if scanRight:
+          bins.append( hists[0].GetBinLowEdge(ibin+1) )
+        else:
+          bins.append( hists[0].GetBinLowEdge(ibin) )
+
+        val[...] = 0
+        err2[...] = 0
+        ctr = 0
+
+    # if last bin would fail threshold, append it to second-to-last
+    if scanRight:
+      edge = hists[0].GetBinLowEdge(nbins)
+    else:
+      edge = hists[0].GetBinLowEdge(1)
+
+    if edge not in bins:
+      # in case histogram has only 1 bin left don't remove it
+      if len(bins) == 1:
+        bins.append(edge)
+      else:
+        bins[len(bins)-1] = edge
+
+    return bins
+
+  if split:
+    # Find bin whose lower edge is zero and run rebinning separately
+    # on positive and negative tails
+    izero = 1
+    for ibin in range(1,nbins):
+      if hists[0].GetBinLowEdge(ibin) >= 0:
+        izero = ibin
+        break
+    print(izero)
+    bins_l = GetBinningInRange(1,izero-1,scanRight=False)
+    bins_r = GetBinningInRange(izero,nbins)
+    # lower edge of izero contained in both lists so remove it from
+    # bins_r when merging
+    return bins_l[::-1] + bins_r[1:]
+  else:
+    imin = 1 # 41 to remove negative Sd0
+    return GetBinningInRange(imin,nbins)
 
 def Rebin(hist,bins,pseudo=False):
   '''
@@ -164,17 +197,17 @@ def RebinHistsAll(region,var):
   bins = []
   h_data = getKey(infile, MyConfig.GetDataHistName(region,var).Data() )
   if args.hist == 'data':
-    bins = GetBinsByStats([h_data], args.stat, args.force)
+    bins = GetBinsByStats([h_data], args.stat, args.force, args.split)
   elif args.hist == 'MC':
     h_mc_vec = []
     for histname in MyConfig.GetMCHistNamesBySys('Nom',region,var):
       h_mc_vec.append(getKey(infile, histname.Data()))
-    bins = GetBinsByStats(h_mc_vec, args.stat, args.force)
+    bins = GetBinsByStats(h_mc_vec, args.stat, args.force, args.split)
   else:
     for flav in ListOfFlavourPairs:
       if args.hist == flav.Data():
         h_mc = getKey(infile, MyConfig.GetMCHistName('Nom',region,flav,var).Data())
-        bins = GetBinsByStats([h_mc], args.stat, args.force)
+        bins = GetBinsByStats([h_mc], args.stat, args.force, args.split)
         break
   if not len(bins):
     print("ERROR: could not define binning in channel "+region.Data()+' '+var.Data())
